895e4166d2f41df79f800320b183c2c00c94b1ba
RafaelDeve <rafaelaybarp@gmail.com>
2024-10-26 14:44:07 -0400

    Creación del proyecto web api

---
+++ api/Models/Book.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Book
+    {
+        public int Id {get;set;}
+        public string Title {get; set;} = string.Empty;
+        public string Genre {get; set;} = string.Empty;
+        public int Year {get;set;} 
+
+
+
+    }
+}
