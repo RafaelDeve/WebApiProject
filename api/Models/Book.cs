602b70773b4278e777665902179dc371d6ec818f
RafaelDeve <rafaelaybarp@gmail.com>
2024-10-26 15:38:23 -0400

    Creación del DBcontext

--- api/Models/Book.cs
+++ api/Models/Book.cs
@@ -10,9 +10,8 @@ namespace api.Models
         public int Id {get;set;}
         public string Title {get; set;} = string.Empty;
         public string Genre {get; set;} = string.Empty;
-        public int Year {get;set;} 
-
-
+        public string Author {get;set;} = string.Empty;
+        public int Year {get;set;}
 
     }
 }
